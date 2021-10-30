d5b2e80dc8cc81486e4c84eb9e4f20f4097f79a3
Paul Chun <84050424+pcdevpc@users.noreply.github.com>
2021-10-30 03:13:34 -0700

    Meta data updated

--- src/components/navbar/index.jsx
+++ src/components/navbar/index.jsx
@@ -1,4 +1,4 @@
-import React, { useState } from "react";
+import React from "react";
 import { BrandLogo } from "../brandLogo";
 import styled from "styled-components";
 import { Button } from "../button";
